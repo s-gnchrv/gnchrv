ff38ee541cb3a0dd2c1895e35d8b8ecb1c0c71e2
Sergey Goncharov <sergon165@icloud.com>
2025-06-30 22:54:33 +0500

    Add naming convention for models

--- backend/app/core/models/base.py
+++ backend/app/core/models/base.py
@@ -1,8 +1,15 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped
-from sqlalchemy.testing.schema import mapped_column
+from sqlalchemy.orm import mapped_column
+
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     id: Mapped[int] = mapped_column(primary_key=True)
