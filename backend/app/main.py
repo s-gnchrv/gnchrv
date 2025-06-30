96bb9050a1b40e58ed52fb7b14d74d35beea405d
Sergey Goncharov <sergon165@icloud.com>
2025-06-30 11:06:17 +0500

    Added config

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,8 +8,12 @@ from api import router as api_router
 app = FastAPI()
 app.include_router(
     api_router,
-    prefix="/api",
+    prefix=settings.api.prefix,
     )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app",
+                reload=True,
+                host=settings.run.host,
+                port=settings.run.port,
+                )
