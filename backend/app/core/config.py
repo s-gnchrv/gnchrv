463a1a3549b36ab945903347c59edcc2c15a566e
Sergey Goncharov <sergon165@icloud.com>
2025-06-30 14:30:32 +0500

    Create DB Config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -9,9 +9,17 @@ class RunConfig(BaseModel):
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
     class Config:
         env_file = ".env"
