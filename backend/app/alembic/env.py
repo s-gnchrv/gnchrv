53863f8e36ad4296c5049637a4bbcdcc388db38a
Sergey Goncharov <sergon165@icloud.com>
2025-07-02 22:11:28 +0500

    Fix: alembic env

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -8,7 +8,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from core.config import settings
-from core.models import Base
+from core import Base
+from core import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
